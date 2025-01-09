05371c574233d0147ce05d315a49c58c1543af51
Joaquin121121 <joaquindelrio121@gmail.com>
2025-01-09 19:32:21 -0300

    9-1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="w-[100vw] h-[100vw] m-auto  bg-red-500"></div>
+    </>
+  );
+}
+
+export default App;
